b34bea2d37d822e046389c2c719a4f4912f47a99
Sean Felling <Snfelling7@gmail.com>
2017-07-12 21:06:08 -0500

    First variables made in PHP

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Practice</title>
+  </head>
+  <body>
+
+  <?php
+  $number1 = 10;
+      $number2 = "100";
+
+      echo $number1 + $number2;
+   ?>
+  </body>
+</html>
